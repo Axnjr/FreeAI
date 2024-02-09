c3e23faa4f704f6000e67acb78f96d084a46b855
Yakshit chhipa <yakshitchhipa@gamil.com>
2024-02-09 10:22:30 +0530

    style: small fixes & tweaks

--- src/app/components/AiCards.tsx
+++ src/app/components/AiCards.tsx
@@ -7,7 +7,7 @@ export default function AiCards() {
         <>
             {
                 modelConfigurations.map((model, id) => {
-                    return <Link key={id} className="mx-4 my-6 shadow-md dark:hover:border-neutral-200 text-left items-start
+                    return <Link key={id} className="mx-6 my-6 shadow-md dark:hover:border-neutral-200 text-left items-start
                     dark:shadow-neutral-700 hover:shadow-2xl w-11/12 md:w-1/3 lg:w-1/4  border-neutral-300 flex flex-col
                     dark:border-neutral-600 rounded-md h-52 bg-neutral-200 dark:bg-neutral-900 justify-center p-4"  
                     href={`/docs/${model.name}`}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ export default async function Home() {
 	return (
 		<>
 			<main className="flex flex-col items-center">
-				<div className="text-center m-auto flex sm:pt-32 lg:pt-20 flex-col items-center 
+				<div className="text-center m-auto flex pt-24 pb-12 sm:pt-32 lg:pt-20 flex-col items-center 
 				justify-center w-fit mb-20 xl:min-h-fit">
 					<h1 className="leading-tighter heading text-center font-black tracking-tight 
 					text-3xl sm:text-5xl md:text-6xl lg:text-6xl lg:mt-6 mt-20 xl:mt-12
-					 text-black dark:text-white">
-						Enable Your apps with <br />
+					 text-black dark:text-white px-3">
+						Enable your apps with <br />
 						<span id="two" style={{lineHeight:"1.5"}}> powerful </span>
 						<span id="thr" 
-						// className="bg-gradient-to-r from-amber-500 via-lime-300 to-blue-600 rounded-xl px-1 text-black"
+						className="bg-gradient-to-r from-amber-500 via-lime-300 to-blue-600 rounded-xl px-2 text-black"
 						> 
 						AI Models</span> for free.
 					</h1>
@@ -25,8 +25,12 @@ export default async function Home() {
 						<br/>
 						<Button className="heading hover:bg-transparent dark:hover:bg-black
 						hover:text-black dark:hover:text-white border-black dark:border-white 
-						hover:border-2 mt-12 sm:mt-4">
-							<a href="#models">Get Started directly.</a>
+						hover:border-2 mt-12 sm:mt-4 mx-2">
+							<a href="#models">Get Started</a>
+						</Button>
+						<Button variant="outline" className="mx-2 border-black dark:border-white 
+						hover:border-2 mt-2">
+							<a href="#models">Give it a Star</a>
 						</Button>
 					</h1>
 				</div>
